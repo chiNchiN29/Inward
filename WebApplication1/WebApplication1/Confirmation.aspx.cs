0499b314f45a5cfc35dd8411ddb89974ef8846a1
chiNchiN29 <chinchin_chua29@yahoo.com>
2015-05-07 15:33:31 +0800

    wew

--- WebApplication1/WebApplication1/Confirmation.aspx.cs
+++ WebApplication1/WebApplication1/Confirmation.aspx.cs
@@ -17,6 +17,9 @@ namespace WebApplication1
 {
     public partial class Confirmation : System.Web.UI.Page
     {
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        SqlCommand cmd;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             SqlDataSource1.SelectCommand = "SELECT check_number AS 'Check Number', customer_name AS Name, customer_address AS Address, contact_number AS 'Contact Number', CHEQUE.account_number AS 'Account Number', CONVERT(VARCHAR(10), check_date, 101) AS Date, amount AS Amount, branch_name AS 'Branch Name', drawee_bank AS 'Drawee Bank', drawee_bank_branch AS 'Drawee Bank Branch', funded AS 'Funded?', verification AS 'Verified?', confirmed AS 'Confirmed?' FROM CHEQUE, CUSTOMER, ACCOUNT, THRESHOLD WHERE CHEQUE.account_number = ACCOUNT.account_number AND ACCOUNT.customer_id = CUSTOMER.customer_id AND ((verification = 'YES' AND amount > maximum) OR verification = 'NO')  ORDER BY CHEQUE.account_number";
@@ -26,7 +29,6 @@ namespace WebApplication1
 
         protected void fundButton_Click(object sender, EventArgs e)
         {
-            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
             connection.Open();
             SqlCommand update = new SqlCommand("update CHEQUE SET confirmed = @fund WHERE account_number = @acctnumber AND check_number = @chknumber", connection);
             update.Parameters.AddWithValue("@acctnumber", GridView1.SelectedRow.Cells[5].Text);
@@ -43,13 +45,12 @@ namespace WebApplication1
 
         protected void unfundButton_Click(object sender, EventArgs e)
         {
-            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
             connection.Open();
-            SqlCommand update = new SqlCommand("update CHEQUE SET confirmed = @fund WHERE account_number = @acctnumber AND check_number = @chknumber", connection);
-            update.Parameters.AddWithValue("@acctnumber", GridView1.SelectedRow.Cells[5].Text);
-            update.Parameters.AddWithValue("@chknumber", GridView1.SelectedRow.Cells[1].Text);
-            update.Parameters.AddWithValue("@fund", "NO");
-            update.ExecuteNonQuery();
+            cmd = new SqlCommand("update CHEQUE SET confirmed = @fund WHERE account_number = @acctnumber AND check_number = @chknumber", connection);
+            cmd.Parameters.AddWithValue("@acctnumber", GridView1.SelectedRow.Cells[5].Text);
+            cmd.Parameters.AddWithValue("@chknumber", GridView1.SelectedRow.Cells[1].Text);
+            cmd.Parameters.AddWithValue("@fund", "NO");
+            cmd.ExecuteNonQuery();
             connection.Close();
             GridView1.DataBind();
             if (GridView1.SelectedRow.RowIndex < GridView1.Rows.Count - 1)
@@ -103,7 +104,7 @@ namespace WebApplication1
         {
             using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
             {
-                using (SqlCommand cmd = new SqlCommand("SELECT check_number AS CheckNo, amount AS Amount, CONVERT(VARCHAR(10), check_date, 111) AS Date, branch_name AS 'Branch Name' drawee_bank AS 'Drawee Bank', drawee_bank_branch AS 'Drawee Bank Branch', funded AS 'Funded?', verification AS 'Verified?', CHEQUE.account_number AS AcctNo, confirmed AS 'Confirmed?' FROM CHEQUE, CUSTOMER, ACCOUNT WHERE CHEQUE.account_number = ACCOUNT.account_number AND ACCOUNT.account_number = CUSTOMER.account_number AND confirmed = 'NO' ORDER BY CHEQUE.account_number"))
+                using (cmd = new SqlCommand("SELECT check_number AS CheckNo, amount AS Amount, CONVERT(VARCHAR(10), check_date, 111) AS Date, branch_name AS 'Branch Name' drawee_bank AS 'Drawee Bank', drawee_bank_branch AS 'Drawee Bank Branch', funded AS 'Funded?', verification AS 'Verified?', CHEQUE.account_number AS AcctNo, confirmed AS 'Confirmed?' FROM CHEQUE, CUSTOMER, ACCOUNT WHERE CHEQUE.account_number = ACCOUNT.account_number AND ACCOUNT.account_number = CUSTOMER.account_number AND confirmed = 'NO' ORDER BY CHEQUE.account_number"))
                 {
                     using (SqlDataAdapter sda = new SqlDataAdapter())
                     {
---
+++ WebApplication1/WebApplication1/UserMaintenance.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="UserMaintenance.aspx.cs" Inherits="WebApplication1.UserMaintenance" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView ID="GridView1" runat="server" datakeynames="user_id"
+        onselectedindexchanged="GridView1_SelectedIndexChanged">
+    </asp:GridView>
+    <br />
+</asp:Content>
