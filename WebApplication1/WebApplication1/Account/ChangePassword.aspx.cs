2150ce98a040786fe263105fe88455366e850407
Jules <juleslui@yahoo.com>
2015-05-08 08:32:00 +0800

    wew fun

--- WebApplication1/WebApplication1/Account/ChangePassword.aspx.cs
+++ WebApplication1/WebApplication1/Account/ChangePassword.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Configuration;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -12,6 +15,24 @@ namespace WebApplication1.Account
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void passChange(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            con.Open();
+            SqlCommand checker = new SqlCommand("SELECT password FROM END_USER WHERE username = '" + Membership.GetUser().UserName + "'", con);
+            if (ChangeUserPassword.CurrentPassword == checker.ExecuteScalar().ToString())
+            {
+                SqlCommand addnew = new SqlCommand("UPDATE END_USER SET password = @pass WHERE username = '" + Membership.GetUser().UserName + "'", con);
+                addnew.Parameters.AddWithValue("@pass", ChangeUserPassword.NewPassword);
+                addnew.ExecuteNonQuery();
+                Response.Redirect("/");
+            }
+            else
+            {
+                Response.Write("Current password is invalid.");
+            }
         }
     }
 }
